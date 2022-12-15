af560680cc4b4d95ccae66cc23a0139e2368f486
CHIDUBEM <52357346+krain9421@users.noreply.github.com>
2022-12-15 10:42:47 +0100

    Update addqueue.c

--- addqueue.c
+++ addqueue.c
@@ -10,31 +10,31 @@
 
 void addqueue(stack_t **head, int n)
 {
-  stack_t *new;
-  stack_t *temp = *head;
+stack_t *new;
+stack_t *temp = *head;
 
-  new = malloc(sizeof(stack_t));
-  if (new == NULL)
-  {
-    printf("Error\n");
-    exit(0);
-  }
-  new->n = n;
+new = malloc(sizeof(stack_t));
+if (new == NULL)
+{
+printf("Error\n");
+exit(0);
+}
+new->n = n;
 
-  if (*head == NULL)
-  {
-    *head = new;
-    (*head)->prev = NULL;
-    (*head)->next = NULL;
-  }
-  else
-  {
-    while (temp->next != NULL)
-    {
-      temp = temp->next;
-    }
-    temp->next = new;
-    new->next = NULL;
-    new->prev = temp;
-  }
+if (*head == NULL)
+{
+*head = new;
+(*head)->prev = NULL;
+(*head)->next = NULL;
+}
+else
+{
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+temp->next = new;
+new->next = NULL;
+new->prev = temp;
+}
 }
