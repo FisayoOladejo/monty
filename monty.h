b5d7d4d7ce032de8f9e34bb912c691848f5e56a7
CHIDUBEM <52357346+krain9421@users.noreply.github.com>
2022-12-15 11:23:40 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -64,7 +64,7 @@ void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
 void _sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
+void _div(stack_t **head, unsigned int counter);
 void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
