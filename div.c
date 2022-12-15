532980fba8831279d199f56290e5f18d61c38db8
CHIDUBEM <52357346+krain9421@users.noreply.github.com>
2022-12-15 11:21:35 +0100

    Update div.c

--- div.c
+++ div.c
@@ -1,19 +1,19 @@
 #include "monty.h"
 /**
- * f_div - divides the top two elements of the stack.
+ * _div - divides the top two elements of the stack.
  *  @head: stack head
  *  @counter: line_number
  *  Return: no return
  */
-void f_div(stack_t **head, unsigned int counter)
+void _div(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+	stack_t *temp;
+	int len = 0;
 
-	h = *head;
-	while (h)
+	temp = *head;
+	while (temp)
 	{
-		h = h->next;
+		temp = temp->next;
 		len++;
 	}
 	if (len < 2)
@@ -24,8 +24,8 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	if (h->n == 0)
+	temp = *head;
+	if (temp->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
@@ -33,8 +33,7 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	temp->next->n = temp->next->n / temp->n;
+	*head = temp->next;
+	free(temp);
 }
